bec17c9f06d4a115a4ffdb4271b3e0573c3b68c6
Jon Gammon <Dolpator@Jons-MacBook-Pro.local>
2016-11-10 16:44:53 -0500

    node-modules

---
+++ src-client/scripts/app.js
@@ -0,0 +1,5 @@
+const ReactDOM = require('react-dom');
+const React = require('react')
+const Backbone = require('backbone');
+
+ReactDOM.render(<h1>Up and REACTING</h1>, document.querySelector('#app-container'));
