95b7a86b1dcd3fd707f1e522db7919a334ddeb28
Jon Gammon <Dolpator@Jons-MacBook-Pro.local>
2016-11-11 16:37:50 -0500

    worked on Errors

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -1,10 +1,11 @@
-const ReactDOM = require('react-dom');
-const React = require('react')
-const Backbone = require('backbone');
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Backbone from 'backbone';
+import $ from 'jquery';
 
 import SubmitView from './submit-view.js'
 import AuthView from './auth-view.js'
-import HomeView from './home-view.js'
+import {HomeView, ProductView} from './home-view.js'
 import {UserModel, UserCollection} from './user-models.js'
 import ACTIONS from './user-actions.js'
 import STORE from './user-store.js'
@@ -12,18 +13,18 @@ import STORE from './user-store.js'
 const AppRouter = Backbone.Router.extend({
   routes: {
      "submit" : "showSubmitView",
-     "auth": "showAuthPage",
-    "": "showHomePage"
+        "auth": "showAuthPage",
+            "": "showHomePage"
   },
 
   showSubmitView: function(){
-     ReactDOM.render(<SubmitView />, document.querySelector('#app-container'))
+     ReactDOM.render(<SubmitView routedFrom = "SubmitView" />, document.querySelector('#app-container'))
    },
   showAuthPage: function(){
-     ReactDom.render(<AuthView />, document.querySelector('#app-container'));
+     ReactDOM.render(<AuthView routedFrom = "AuthView"/>, document.querySelector('#app-container'));
    },
   showHomePage: function(){
-     ReactDOM.render(<HomeView />, document.querySelector('#app-container'));
+     ReactDOM.render(<HomeView routedFrom = "HomeView"/>, document.querySelector('#app-container'));
   },
 
   initialize: function(){
