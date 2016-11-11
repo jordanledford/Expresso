60a6da3b32cc459dc2963eecdd98743a3d5e03c6
Jon Gammon <Dolpator@Jons-MacBook-Pro.local>
2016-11-11 08:38:52 -0500

    Added InfoView, SubmitView, AuthView, with basic layout

---
+++ src-client/scripts/auth-view.js
@@ -0,0 +1,29 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const Backbone = require('backbone')
+
+
+const AuthView = React.createClass({
+
+   render: function(){
+
+      return(
+               <div class="row text-center">
+                  <form class="col-sm-offset-3 col-sm-6 " id="auth-form">
+                        <a href="#"><i class="fa fa-home fa-2x " aria-hidden="true"></i></a>
+                        <h2 class="">Login or Sign Up</h2>
+                     <div class="form-group">
+                        <label for=name">Name</label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter Name">
+                     </div>
+                     <div class="form-group">
+                        <label for="pw">Password</label>
+                        <input type="password" class="form-control" id="password" placeholder="Enter Password">
+                     </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                  </form>
+               </div>
+            )
+         }
+})
+module.export = AuthView
