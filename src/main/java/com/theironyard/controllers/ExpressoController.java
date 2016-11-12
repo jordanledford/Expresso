2ae0aa8deec810cfa4308fa89e6362a8399d4b5e
rdw1995 <whittlebekah@gmail.com>
2016-11-12 13:10:53 -0500

    re-writing routes

--- src/main/java/com/theironyard/controllers/ExpressoController.java
+++ src/main/java/com/theironyard/controllers/ExpressoController.java
@@ -2,7 +2,6 @@ package com.theironyard.controllers;
 
 import com.theironyard.entities.Shop;
 import com.theironyard.entities.User;
-import com.theironyard.services.LikeRepo;
 import com.theironyard.services.ShopRepo;
 import com.theironyard.services.UserRepo;
 import com.theironyard.utitilies.PasswordStorage;
@@ -48,7 +47,7 @@ public class ExpressoController {
         if (shops.count() == 0) {
             User user = new User("Bekah", PasswordStorage.createHash("TrumpTrain"));
             users.save(user);
-            shops.save(new Shop("Collective Coffee Co", "Mt. Pleasant, SC", "7am - 5pm", "http://collective-coffee.com/", "https://static.squarespace.com/static/50f1db62e4b0ceb75bca6fdd/t/51044ad8e4b00bb1fb60cc11/1359235800677/Counter%20Collective%20Fix-3220%20bg.jpg", "A modern feel with quaint taste."))
+            shops.save(new Shop("Collective Coffee Co", "Mt. Pleasant, SC", "7am - 5pm", "http://collective-coffee.com/", "https://static.squarespace.com/static/50f1db62e4b0ceb75bca6fdd/t/51044ad8e4b00bb1fb60cc11/1359235800677/Counter%20Collective%20Fix-3220%20bg.jpg", "A modern feel with quaint taste."));
         }
     }
 
@@ -74,7 +73,7 @@ public class ExpressoController {
     @RequestMapping(path = "/user", method = RequestMethod.GET)
     public User getUser (HttpSession session){
         String name = (String) session.getAttribute("name");
-        return users.findFirstByName("name");
+        return users.findFirstByName(name);
     }
 
     @RequestMapping(path = "/shops", method = RequestMethod.POST)
@@ -85,6 +84,27 @@ public class ExpressoController {
         }
         shop.setUser(users.findFirstByName(name));
         return new ResponseEntity<Shop>(shops.save(shop), HttpStatus.OK);
+    }
+
+//    @RequestMapping(path = "/edit-shops", method = RequestMethod.POST)
+//    public ResponseEntity<Shop> editShop (HttpSession session, @RequestBody Shop shop){
+//
+//    }
+//
+//    @RequestMapping(path = "/delete-shops", method = RequestMethod.POST)
+//    public ResponseEntity<Shop> deleteShop (HttpSession session, @RequestBody Shop shop){
+//
+//    }
+
+
+    @RequestMapping(path = "/shops", method = RequestMethod.GET)
+    public List<Shop> getShops (HttpSession session) {
+        String name = (String) session.getAttribute("name");
+        User user = users.findFirstByName(name);
+
+        ArrayList<Shop> allShops = new ArrayList<>();
+        shops.save(allShops);
+        return allShops;
     }
 }
 
