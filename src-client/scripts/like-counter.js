74841f0617c6d0f26a570040f6cda6db752f17b1
Jon Gammon <Dolpator@Jons-MacBook-Pro.local>
2016-11-14 11:56:28 -0500

    working on like function

--- src-client/scripts/like-counter.js
+++ src-client/scripts/like-counter.js
@@ -6,14 +6,7 @@ import $ from 'jquery';
 
 const LikeCounter = function(evt){
   // Add .onclick to react FA El not here
-  getInitialState: function() {
-     let startingState = {
-        votes: 0,
 
-     }
-     return startingState
-  },
-  
 
   let Thumb = document.getElementsByClassName('fa')
   Thumb.onClick()
